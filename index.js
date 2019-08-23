9674c850c8dcfc82cbb5ec5f6bc552e680441b5d
Madeleine Daly <mdaly@invaluable.com>
2019-08-23 12:59:06 -0400

    handle case of no remotes

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ templateSettings.interpolate = /{([\s\S]+?)}/g;
 module.exports = async ({commit, remote, cwd, gitDir}) => {
   const config = await gitconfig(cwd, {gitDir});
 
-  if (!(remote in config.remote)) {
-    throw new Error(`remote "${remote}" not found in ${cwd}/${gitDir}/config`);
+  if (!config.remote) {
+    throw new Error(`No remotes found in ${cwd}/${gitDir}/config`);
+  }
+
+  if (!config.remote[remote]) {
+    throw new Error(`Remote "${remote}" not found in ${cwd}/${gitDir}/config`);
   }
 
   const {source, owner, name} = gitUrlParse(config.remote[remote].url);
   const templates = require('./templates.json');
 
-  if (!(source in templates)) {
-    throw new Error(`service "${source}" is not supported`);
+  if (!templates[source]) {
+    throw new Error(`Service "${source}" is not supported`);
   }
 
   const interpolate = template(templates[source]);
